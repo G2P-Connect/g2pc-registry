3b3309f6627ba06dc765fb41512b7338d13a4be8
“abhilash_k@tekditechnologies.com” <abhilash_k@tekditechnologies.com>
2023-12-11 23:30:38 +0530

    g2pc-dp-core-lib changes.

--- g2pc-dp-core-lib/src/main/java/g2pc/dp/core/lib/G2pcDpCoreLibraryApplication.java
+++ g2pc-dp-core-lib/src/main/java/g2pc/dp/core/lib/G2pcDpCoreLibraryApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"g2pc.core.lib","g2pc.dp.core.lib"})
+@ComponentScan({"g2pc.core.lib","g2pc.dp.core.lib","g2pc.dp.core.lib.serviceimpl",
+				"g2pc.dp.core.lib.repository"})
 public class G2pcDpCoreLibraryApplication {
 
 	public static void main(String[] args) {
