58205af2052bf08294fd6f7bba2a159c1e3b4ab5
“abhilash_k@tekditechnologies.com” <abhilash_k@tekditechnologies.com>
2024-02-14 13:46:43 +0530

    g2pc-ref-farmer-regsvc changes.

--- g2pc-reference-apps/g2pc-ref-farmer-regsvc/src/main/java/g2pc/ref/farmer/regsvc/serviceimpl/FarmerResponseBuilderServiceImpl.java
+++ g2pc-reference-apps/g2pc-ref-farmer-regsvc/src/main/java/g2pc/ref/farmer/regsvc/serviceimpl/FarmerResponseBuilderServiceImpl.java
@@ -1,10 +1,9 @@
 package g2pc.ref.farmer.regsvc.serviceimpl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import g2pc.core.lib.dto.common.header.RequestHeaderDTO;
-import g2pc.core.lib.dto.common.header.ResponseHeaderDTO;
-import g2pc.core.lib.dto.common.message.request.QueryDTO;
-import g2pc.core.lib.dto.common.message.response.DataDTO;
+import g2pc.core.lib.dto.search.message.request.QueryDTO;
+import g2pc.core.lib.service.ElasticsearchService;
 import g2pc.ref.farmer.regsvc.dto.request.QueryParamsFarmerDTO;
 import g2pc.ref.farmer.regsvc.dto.response.RegRecordFarmerDTO;
 import g2pc.ref.farmer.regsvc.entity.FarmerInfoEntity;
@@ -12,7 +11,9 @@ import g2pc.ref.farmer.regsvc.repository.FarmerInfoRepository;
 import g2pc.ref.farmer.regsvc.service.FarmerResponseBuilderService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.action.search.SearchResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -22,8 +23,14 @@ import java.util.*;
 @Slf4j
 public class FarmerResponseBuilderServiceImpl implements FarmerResponseBuilderService {
 
+    @Value("${sunbird.enabled}")
+    private Boolean sunbirdEnabled;
+
     @Autowired
     private FarmerInfoRepository farmerInfoRepository;
+
+    @Autowired
+    private ElasticsearchService elasticsearchService;
 
     /**
      * Get farmer records information from DB
@@ -32,7 +39,7 @@ public class FarmerResponseBuilderServiceImpl implements FarmerResponseBuilderSe
      * @return Farmer records
      */
     @Override
-    public RegRecordFarmerDTO getRegRecordFarmerDTO(FarmerInfoEntity farmerInfoEntity) {
+    public RegRecordFarmerDTO getRegRecordFarmerDTOFromDb(FarmerInfoEntity farmerInfoEntity) {
         RegRecordFarmerDTO dto = new RegRecordFarmerDTO();
         dto.setFarmerId(farmerInfoEntity.getFarmerId());
         dto.setFarmerName(farmerInfoEntity.getFarmerName());
@@ -41,6 +48,19 @@ public class FarmerResponseBuilderServiceImpl implements FarmerResponseBuilderSe
         dto.setPaymentDate(farmerInfoEntity.getPaymentDate());
         dto.setPaymentAmount(farmerInfoEntity.getPaymentAmount());
         return dto;
+    }
+
+    /**
+     * Get farmer records information from Sunbird
+     *
+     * @param searchResponse required
+     * @return Farmer records
+     */
+    @Override
+    public RegRecordFarmerDTO getRegRecordFarmerDTOFromSunbird(SearchResponse searchResponse) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String sourceAsString = searchResponse.getHits().getHits()[0].getSourceAsString();
+        return objectMapper.readValue(sourceAsString, RegRecordFarmerDTO.class);
     }
 
     /**
@@ -58,12 +78,27 @@ public class FarmerResponseBuilderServiceImpl implements FarmerResponseBuilderSe
             QueryParamsFarmerDTO queryParamsFarmerDTO = objectMapper.readValue(queryParams, QueryParamsFarmerDTO.class);
             String farmerId = queryParamsFarmerDTO.getFarmerId();
             String season = queryParamsFarmerDTO.getSeason();
-            Optional<FarmerInfoEntity> optional = farmerInfoRepository.findBySeasonAndFarmerId(season, farmerId);
-            if (optional.isPresent()) {
-                RegRecordFarmerDTO regRecordFarmerDTO = getRegRecordFarmerDTO(optional.get());
-                regFarmerRecordsList.add(objectMapper.writeValueAsString(regRecordFarmerDTO));
+            if (Boolean.TRUE.equals(sunbirdEnabled)) {
+                log.info("Record fetched from sunbird");
+                Map<String, String> fieldValues = new HashMap<>();
+                fieldValues.put("farmer_id.keyword", farmerId);
+                fieldValues.put("season.keyword", season);
+                SearchResponse response = elasticsearchService.exactSearch("farmer_info", fieldValues);
+                if (response.getHits().getHits().length > 0) {
+                    RegRecordFarmerDTO regRecordFarmerDTO = getRegRecordFarmerDTOFromSunbird(response);
+                    regFarmerRecordsList.add(objectMapper.writeValueAsString(regRecordFarmerDTO));
+                } else {
+                    regFarmerRecordsList.add(StringUtils.EMPTY);
+                }
             } else {
-                regFarmerRecordsList.add(StringUtils.EMPTY);
+                log.info("Record fetched from postgres");
+                Optional<FarmerInfoEntity> optional = farmerInfoRepository.findBySeasonAndFarmerId(season, farmerId);
+                if (optional.isPresent()) {
+                    RegRecordFarmerDTO regRecordFarmerDTO = getRegRecordFarmerDTOFromDb(optional.get());
+                    regFarmerRecordsList.add(objectMapper.writeValueAsString(regRecordFarmerDTO));
+                } else {
+                    regFarmerRecordsList.add(StringUtils.EMPTY);
+                }
             }
         }
         return regFarmerRecordsList;
--- g2pc-reference-apps/g2pc-ref-farmer-regsvc/src/main/java/g2pc/ref/farmer/regsvc/serviceimpl/FarmerValidationServiceImpl.java
+++ g2pc-reference-apps/g2pc-ref-farmer-regsvc/src/main/java/g2pc/ref/farmer/regsvc/serviceimpl/FarmerValidationServiceImpl.java
@@ -10,16 +10,18 @@ import com.networknt.schema.ValidationMessage;
 import g2pc.core.lib.constants.CoreConstants;
 import g2pc.core.lib.constants.G2pSecurityConstants;
 import g2pc.core.lib.dto.common.header.RequestHeaderDTO;
-import g2pc.core.lib.dto.common.message.request.QueryDTO;
-import g2pc.core.lib.dto.common.message.request.RequestDTO;
-import g2pc.core.lib.dto.common.message.request.RequestMessageDTO;
-import g2pc.core.lib.dto.common.message.request.SearchRequestDTO;
+import g2pc.core.lib.dto.search.message.request.QueryDTO;
+import g2pc.core.lib.dto.search.message.request.RequestDTO;
+import g2pc.core.lib.dto.search.message.request.RequestMessageDTO;
+import g2pc.core.lib.dto.status.message.request.StatusRequestDTO;
+import g2pc.core.lib.dto.status.message.request.StatusRequestMessageDTO;
 import g2pc.core.lib.enums.ExceptionsENUM;
 import g2pc.core.lib.exceptions.G2pHttpException;
 import g2pc.core.lib.exceptions.G2pcError;
 import g2pc.core.lib.exceptions.G2pcValidationException;
 import g2pc.core.lib.security.service.AsymmetricSignatureService;
 import g2pc.core.lib.security.service.G2pEncryptDecrypt;
+import g2pc.core.lib.security.service.G2pcUtilityClass;
 import g2pc.dp.core.lib.service.RequestHandlerService;
 import g2pc.ref.farmer.regsvc.constants.Constants;
 import g2pc.ref.farmer.regsvc.dto.request.QueryFarmerDTO;
@@ -34,6 +36,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.SignatureException;
 import java.util.*;
 
@@ -72,6 +75,9 @@ public class FarmerValidationServiceImpl implements FarmerValidationService {
     @Value("${crypto.from_dc.key_path}")
     private String farmer_key_path;
 
+    @Autowired
+    G2pcUtilityClass g2pcUtilityClass;
+
     /**
      * Validate request dto.
      *
@@ -86,57 +92,17 @@ public class FarmerValidationServiceImpl implements FarmerValidationService {
                 QueryFarmerDTO.class, QueryParamsFarmerDTO.class);
         byte[] json = objectMapper.writeValueAsBytes(requestDTO.getMessage());
         RequestMessageDTO messageDTO =  objectMapper.readValue(json, RequestMessageDTO.class);
-        List<SearchRequestDTO> searchRequestList = messageDTO.getSearchRequest();
-        for(SearchRequestDTO searchRequestDTO : searchRequestList){
-            String queryString = objectMapper.writeValueAsString(searchRequestDTO.getSearchCriteria().getQuery());
-            QueryDTO queryFarmerDTO = objectMapper.readerFor(QueryDTO.class).
-                    readValue(queryString);
-            validateQueryDto(queryFarmerDTO);
-        }
         String headerString = new ObjectMapper()
                 .writerWithDefaultPrettyPrinter()
                 .writeValueAsString(requestDTO.getHeader());
-        RequestHeaderDTO headerDTO = objectMapper.readerFor(RequestHeaderDTO.class).
-                readValue(headerString);
-        requestHandlerService.validateRequestHeader(headerDTO);
-        requestHandlerService.validateRequestMessage(messageDTO);
-    }
-
-    /**
-     * Validate query dto.
-     *
-     * @param queryFarmerDTO the query farmer dto
-     * @throws G2pcValidationException     the validation exception
-     * @throws JsonProcessingException the json processing exception
-     */
-    @Override
-    public void validateQueryDto(QueryDTO queryFarmerDTO) throws JsonProcessingException, G2pcValidationException {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        log.info("Query object -> " + queryFarmerDTO);
-        String queryString = new ObjectMapper()
+        g2pcUtilityClass.validateResponse(headerString , CoreConstants.REQUEST_HEADER);
+        String messageString = new ObjectMapper()
                 .writerWithDefaultPrettyPrinter()
-                .writeValueAsString(queryFarmerDTO);
-        log.info("Query String" + queryString);
-        InputStream schemaStreamQuery = FarmerValidationServiceImpl.class.getClassLoader()
-                .getResourceAsStream("schema/farmerQuerySchema.json");
-        JsonNode jsonNode = objectMapper.readTree(queryString);
-        JsonSchema schema = null;
-        if (schemaStreamQuery != null) {
-            schema = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V4).
-                    getSchema(schemaStreamQuery);
-        }
-        Set<ValidationMessage> errorMessage = schema.validate(jsonNode);
-        List<G2pcError> errorcombinedMessage = new ArrayList<>();
-        for (ValidationMessage error : errorMessage) {
-            log.info("Validation errors" + error);
-            errorcombinedMessage.add(new G2pcError("", error.getMessage()));
-
-        }
-        if (errorMessage.size() > 0) {
-            throw new G2pcValidationException(errorcombinedMessage);
-        }
+                .writeValueAsString(messageDTO);
+        g2pcUtilityClass.validateResponse(messageString,CoreConstants.SEARCH_REQUEST);
     }
+
+
 
     /**
      * Method to validate signature and encrypted message
@@ -150,6 +116,7 @@ public class FarmerValidationServiceImpl implements FarmerValidationService {
 
         ObjectMapper objectMapper = new ObjectMapper();
         RequestMessageDTO messageDTO;
+        Boolean isMsgEncrypted = requestDTO.getHeader().getIsMsgEncrypted();
         if(isSign){
             if(!metaData.get(CoreConstants.IS_SIGN).equals(true)){
                 throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(), Constants.CONFIGURATION_MISMATCH_ERROR));
@@ -157,7 +124,7 @@ public class FarmerValidationServiceImpl implements FarmerValidationService {
             Resource resource = resourceLoader.getResource(farmer_key_path);
             InputStream fis = resource.getInputStream();
             if(isEncrypt){
-                if(!requestDTO.getHeader().getIsMsgEncrypted()){
+                if(!isMsgEncrypted){
                     throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
                 }
 
@@ -176,11 +143,11 @@ public class FarmerValidationServiceImpl implements FarmerValidationService {
                     log.info("Rejecting the on-search request in farmer as signature is not valid");
                     throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_SIGNATURE_INVALID.toValue(), e.getMessage()));
                 }
-                if(requestDTO.getHeader().getIsMsgEncrypted()){
+                if(isMsgEncrypted){
                     String deprecatedMessageString;
                     try{
                         deprecatedMessageString= encryptDecrypt.g2pDecrypt(messageString, G2pSecurityConstants.SECRET_KEY);
-                    } catch (RuntimeException e ){
+                    } catch (RuntimeException | InvalidAlgorithmParameterException e ){
                         log.info("Rejecting the on-search request in farmer as signature is not valid");
                         throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_ENCRYPTION_INVALID.toValue(), "Error in Encryption/Decryption"));
                     }
@@ -191,7 +158,7 @@ public class FarmerValidationServiceImpl implements FarmerValidationService {
                     throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
                 }
             }else{
-                if(requestDTO.getHeader().getIsMsgEncrypted()){
+                if(isMsgEncrypted){
                     throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
                 }
                 byte[] json = objectMapper.writeValueAsBytes(requestDTO.getMessage());
@@ -217,7 +184,106 @@ public class FarmerValidationServiceImpl implements FarmerValidationService {
                 throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
             }
             if(isEncrypt){
-                if(!requestDTO.getHeader().getIsMsgEncrypted()){
+                if(!isMsgEncrypted){
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
+                }
+                String messageString = requestDTO.getMessage().toString();
+                String deprecatedMessageString;
+                try{
+                    deprecatedMessageString= encryptDecrypt.g2pDecrypt(messageString,G2pSecurityConstants.SECRET_KEY);
+                } catch (RuntimeException | InvalidAlgorithmParameterException e ){
+                    log.info("Rejecting the on-search request in farmer as signature is not valid");
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_ENCRYPTION_INVALID.toValue(), "Error in Encryption/Decryption"));
+                }
+                log.info("Decrypted Message string ->"+deprecatedMessageString);
+                messageDTO  = objectMapper.readerFor(RequestMessageDTO.class).
+                        readValue(deprecatedMessageString);
+
+            }else{
+                if(isMsgEncrypted){
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
+                }
+                byte[] json = objectMapper.writeValueAsBytes(requestDTO.getMessage());
+                messageDTO =  objectMapper.readValue(json, RequestMessageDTO.class);
+            }
+        }
+        requestDTO.setMessage(messageDTO);
+        return messageDTO;
+    }
+
+    @Override
+    public StatusRequestMessageDTO signatureValidation(Map<String, Object> metaData, StatusRequestDTO requestDTO) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        StatusRequestMessageDTO messageDTO;
+        Boolean isMsgEncrypted = requestDTO.getHeader().getIsMsgEncrypted();
+        if(isSign){
+            if(!metaData.get(CoreConstants.IS_SIGN).equals(true)){
+                throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(), Constants.CONFIGURATION_MISMATCH_ERROR));
+            }
+            Resource resource = resourceLoader.getResource(farmer_key_path);
+            InputStream fis = resource.getInputStream();
+            if(isEncrypt){
+                if(!isMsgEncrypted){
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
+                }
+
+                String requestHeaderString = objectMapper.writeValueAsString(requestDTO.getHeader());
+                String requestSignature = requestDTO.getSignature();
+                String messageString = requestDTO.getMessage().toString();
+                String data = requestHeaderString+messageString;
+                try{if(! asymmetricSignatureService.verifySignature(data.getBytes(), Base64.getDecoder().decode(requestSignature) , fis ,p12Password) ){
+                    log.info("Rejecting the on-search request in farmer as signature is not valid");
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_SIGNATURE_INVALID.toValue(), "signature is not valid "));
+                }}catch(SignatureException e){
+                    log.info("Rejecting the on-search request in farmer as signature is not valid");
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_SIGNATURE_INVALID.toValue(), "signature is not valid ->"+e.getMessage()));
+                }
+                catch(IOException e){
+                    log.info("Rejecting the on-search request in farmer as signature is not valid");
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_SIGNATURE_INVALID.toValue(), e.getMessage()));
+                }
+                if(isMsgEncrypted){
+                    String deprecatedMessageString;
+                    try{
+                        deprecatedMessageString= encryptDecrypt.g2pDecrypt(messageString, G2pSecurityConstants.SECRET_KEY);
+                    } catch (RuntimeException e ){
+                        log.info("Rejecting the on-search request in farmer as signature is not valid");
+                        throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_ENCRYPTION_INVALID.toValue(), "Error in Encryption/Decryption"));
+                    }
+                    log.info("Decrypted Message string ->"+deprecatedMessageString);
+                    messageDTO  = objectMapper.readerFor(StatusRequestMessageDTO.class).
+                            readValue(deprecatedMessageString);
+                } else {
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
+                }
+            }else{
+                if(isMsgEncrypted){
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
+                }
+                byte[] json = objectMapper.writeValueAsBytes(requestDTO.getMessage());
+                messageDTO =  objectMapper.readValue(json, StatusRequestMessageDTO.class);
+                String requestHeaderString = objectMapper.writeValueAsString(requestDTO.getHeader());
+                String requestSignature = requestDTO.getSignature();
+                String messageString = objectMapper.writeValueAsString(messageDTO);
+                String data = requestHeaderString+messageString;
+                log.info("Signature ->"+requestSignature);
+                try{if(! asymmetricSignatureService.verifySignature(data.getBytes(), Base64.getDecoder().decode(requestSignature) , fis ,p12Password) ){
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_SIGNATURE_INVALID.toValue(), "signature is not valid "));
+                }}catch(SignatureException e){
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_SIGNATURE_INVALID.toValue(), "signature is not valid "));
+                }
+                catch(IOException e){
+                    log.info("Rejecting the on-search request in farmer as signature is not valid");
+                    throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_SIGNATURE_INVALID.toValue(), "signature is not valid "));
+                }
+
+            }
+        } else {
+            if(!metaData.get(CoreConstants.IS_SIGN).equals(false)){
+                throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
+            }
+            if(isEncrypt){
+                if(!isMsgEncrypted){
                     throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
                 }
                 String messageString = requestDTO.getMessage().toString();
@@ -229,19 +295,36 @@ public class FarmerValidationServiceImpl implements FarmerValidationService {
                     throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_ENCRYPTION_INVALID.toValue(), "Error in Encryption/Decryption"));
                 }
                 log.info("Decrypted Message string ->"+deprecatedMessageString);
-                messageDTO  = objectMapper.readerFor(RequestMessageDTO.class).
+                messageDTO  = objectMapper.readerFor(StatusRequestMessageDTO.class).
                         readValue(deprecatedMessageString);
 
             }else{
-                if(requestDTO.getHeader().getIsMsgEncrypted()){
+                if(isMsgEncrypted){
                     throw new G2pHttpException(new G2pcError(ExceptionsENUM.ERROR_VERSION_NOT_VALID.toValue(),Constants.CONFIGURATION_MISMATCH_ERROR));
                 }
                 byte[] json = objectMapper.writeValueAsBytes(requestDTO.getMessage());
-                messageDTO =  objectMapper.readValue(json, RequestMessageDTO.class);
+                messageDTO =  objectMapper.readValue(json, StatusRequestMessageDTO.class);
             }
         }
         requestDTO.setMessage(messageDTO);
         return messageDTO;
     }
+
+    @Override
+    public void validateStatusRequestDTO(StatusRequestDTO statusRequestDTO) throws IOException, G2pcValidationException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        byte[] json = objectMapper.writeValueAsBytes(statusRequestDTO.getMessage());
+        StatusRequestMessageDTO statusRequestMessageDTO =  objectMapper.readValue(json, StatusRequestMessageDTO.class);
+        String headerString = new ObjectMapper()
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(statusRequestDTO.getHeader());
+        g2pcUtilityClass.validateResponse(headerString , CoreConstants.REQUEST_HEADER);
+        String messageString = new ObjectMapper()
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(statusRequestMessageDTO);
+        g2pcUtilityClass.validateResponse(messageString,CoreConstants.STATUS_REQUEST);
+    }
+
+
 }
 
